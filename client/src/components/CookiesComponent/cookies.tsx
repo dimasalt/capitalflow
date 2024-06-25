87d3dcff0aaa39f487ab71afa10a0dbf81868955
dimasalt <141041615+dimasalt@users.noreply.github.com>
2024-06-25 15:41:26 -0400

    Started working on pages and created navbar

---
+++ client/src/components/CookiesComponent/cookies.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import cookies from 'js-cookie';
+import crypto from 'crypto';
+
+
+/**
+ * -----------------------------------------------------------------
+ * @description sets or renews (if exist) a cookie with use token
+ * @returns null
+ * -----------------------------------------------------------------
+ */
+const CookiesComponent = () => {   
+
+    // user token
+    let token: string;
+
+    //if cookie doesn't exit create new token
+    if (cookies.get('token') === undefined)
+    {
+        let appKey:string = process.env.DATA_API_KEY?.toString() ?? '';
+        let random:string = Math.random().toString(36).substring(2);
+        
+        token = crypto.createHash('sha256').update(appKey + random).digest('hex');
+    }       
+    else { // otherwise just read the token from existing cookie
+        token = cookies.get('token')!;         
+    }
+
+    //set cookie
+    cookies.set('token', token, { expires: 30, path: '/', sameSite: 'strict' } );     
+  
+    return null;  
+}
+
+export default CookiesComponent;
--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -25,10 +25,10 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="flex justify-around shadow-md bg-slate-800 py-4 w-full">
+            <nav className="flex justify-around shadow-md bg-cyan-900 py-4 w-full">
                 {/* <!-- logo and app name --> */}
                 <div className=' w-full md:w-fit'>
-                    <Link className=" text-gray-300 font-semibold text-2xl md:text-3xl" href="/">
+                    <Link className=" text-gray-200 font-semibold text-2xl md:text-3xl" href="/">
                         <span className=' text-green-600 ms-3'>Capital</span>Flow
                     </Link>                          
                 </div>                    
@@ -36,19 +36,19 @@ const Navbar = () => {
                 {/* <!-- an actual middle menu --> */}
                 <div className=" text-gray-400 me-10 justify-center mt-2 text-lg hidden md:flex">    
                 <Link className="hover:text-gray-100 me-8" id='test' href="/">
-                        <i className="bi bi-calculator me-1"></i>
+                        <i className="bi bi-house me-1"></i>                       
                         Home
                     </Link>                   
-                    <Link className="hover:text-gray-100 me-8" id='test' href="/">
-                        <i className="bi bi-calculator me-1"></i>
+                    <Link className="hover:text-gray-100 me-8" id='test' href="/records">
+                        <i className="bi bi-bank me-1"></i>
                         Records
                     </Link>
-                    <Link className="hover:text-gray-100 me-8" href="/feeds">
-                        <i className="bi bi-flower2 me-1"></i>
+                    <Link className="hover:text-gray-100 me-8" href="/budgets">
+                        <i className="bi bi-cash-coin me-1"></i>
                         Budgets
                     </Link>    
-                    <Link className="hover:text-gray-100" href="/ration">
-                        <i className="bi bi-egg me-1"></i>
+                    <Link className="hover:text-gray-100" href="/contacts">
+                        <i className="bi bi-file-earmark-person me-1"></i>
                         Contacts
                     </Link>                                                                          
                 </div>
@@ -70,28 +70,36 @@ const Navbar = () => {
             </nav>    
 
             {/* <!-- visible only on small screens and when button is clicked --> */}
-            <ul className='bg-neutral-600 text-gray-400 py-2 flex-row justify-items-center w-full hidden md:hidden shadow-sm' id="smallMenu">
-                <li>
+            <ul className='bg-neutral-700 text-gray-200 py-2 flex-row justify-items-center w-full hidden md:hidden shadow-sm' id="smallMenu">
+                <li>                    
                     <div className='w-full flex justify-start ms-10'>
                         <Link className="hover:text-gray-100 me-8" id='test' href="/">
-                            <i className="bi bi-calculator me-1"></i>
-                            Calculator
+                            <i className="bi bi-house me-1"></i>
+                            Home
+                        </Link>
+                    </div>
+                </li>
+                <li>                    
+                    <div className='w-full flex justify-start ms-10'>
+                        <Link className="hover:text-gray-100 me-8" id='test' href="/records">
+                            <i className="bi bi-bank me-1"></i>
+                            Records
                         </Link>
                     </div>
                 </li>
                 <li>
                     <div className='w-full flex justify-start ms-10'>
-                        <Link className="hover:text-gray-100 me-8" href="/feeds">
-                            <i className="bi bi-flower2 me-1"></i>
-                            Feeds
+                        <Link className="hover:text-gray-100 me-8" href="/budgets">
+                            <i className="bi bi-cash-coin me-1"></i>
+                            Budgets
                         </Link>    
                     </div>
                 </li>
                 <li>
                     <div className='w-full flex justify-start ms-10'>
-                        <Link className="hover:text-gray-100 me-8" href="/ration">
-                            <i className="bi bi-egg me-1"></i> 
-                            Ration
+                        <Link className="hover:text-gray-100 me-8" href="/contacts">
+                            <i className="bi bi-file-earmark-person me-1"></i> 
+                            Contacts
                         </Link>  
                     </div>                    
                 </li>
